82bbd7c5861395a9e9499b0274b28eab9bce56f0
Faraz <104452510+GeekkyCoder@users.noreply.github.com>
2022-11-04 21:35:04 +0500

    fixed animation glitch on tech stack and fixed the visibility of menu icon

--- javascript/about.js
+++ javascript/about.js
@@ -4,11 +4,9 @@ const slideObserverEl = document.querySelectorAll('.slide-up')
 const slideObserver = new IntersectionObserver((entries)=> {
     entries.forEach(entry=> {
        if(entry.isIntersecting){
-        console.log("intersecting")
          entry.target.classList.add("slide-down")
          entry.target.classList.remove("slide-up")
        }else {
-        console.log("not intersecting")
          entry.target.classList.remove("slide-down")
          entry.target.classList.add("slide-up")
        }
